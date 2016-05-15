7b9d0623806584ebb2d1ef9c45abae92456ced51
BestLine <Falllenman@hotmail.com>
2016-05-15 19:46:54 +0300

    добавил комменты

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp        = require('gulp'),
+var gulp        = require('gulp'),// сюда прописываются требуемые либы
 	sass        = require('gulp-sass'),
 	browserSync = require('browser-sync');
 
-gulp.task('sass', function() {
-	return gulp.src('app/sass/**/*.sass')
-	.pipe(sass())
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.reload({stream: true}))
+gulp.task('sass', function() { // создание задачи для гульпа
+	return gulp.src('app/sass/**/*.sass')  // файлы которые берёт гульп
+	.pipe(sass()) // действие производимое над файлами
+	.pipe(gulp.dest('app/css')) // папка куда выгружаются файлы
+	.pipe(browserSync.reload({stream: true})) // дополнительная комманда для перезагрузки браузера
 });
 
 gulp.task('browser-sync', function() {
